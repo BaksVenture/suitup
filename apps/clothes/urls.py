7643f58af70a1520bee8f2e68937e0cdb231ed46
saygon91 <saygon91@gmail.com>
2012-05-11 10:22:08 +0600

    dodelay payment i map href 4tob rabotal

--- apps/clothes/urls.py
+++ apps/clothes/urls.py
@@ -4,4 +4,7 @@ urlpatterns = patterns('',
     url(r'^$', 'clothes.views.index'),
     url(r'^brand/(\d+)/$', 'clothes.views.brand_page'),
     url(r'^brand/catalog/(\d+)/$', 'clothes.views.brand_catalog'),
+    url(r'^help/$', 'clothes.views.help'),
+    url(r'^about/$', 'clothes.views.about'),
+    url(r'^contacts/$', 'clothes.views.contacts'),
 )
